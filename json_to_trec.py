ebb4761e5d0c5ae8ddbea5714bf06219ccf801d2
Manikandan <mtiruman@buffalo.edu>
2015-11-16 18:43:37 -0500

    Got first VSM and BM25 results

--- json_to_trec.py
+++ json_to_trec.py
@@ -10,23 +10,27 @@ import json
 import urllib2
 
 
-# change the url according to your own koding username and query
-inurl = 'http://meganarutokage.koding.io:8983/solr/eval/select?q=*Russia%27s+intervention+in+Syria*&fl=id%2Cscore&wt=json&indent=true&rows=1000'
-outfn = 'myresult.txt'
+with open('test_queries.txt', 'rb') as queries:
+    # change the url according to your own koding username and query
+    qid = 1
+    for line in queries:
+        query = line[12:].rstrip()
+        inurl= "http://meganarutokage.koding.io:8983/solr/eval/select?q=*" + query + "*&fl=id%2Cscore&wt=json&indent=true&rows=1000"
+        outfn = 'BM25results.txt'
 
+        # change query id and IRModel name accordingly
+        IRModel= 'BM25'
+        outf = open(outfn, 'a+')
+        data = urllib2.urlopen(inurl)
+        # if you're using python 3, you should use
+        # data = urllib.request.urlopen(inurl)
 
-# change query id and IRModel name accordingly
-qid = '001'
-IRModel='default'
-outf = open(outfn, 'a+')
-data = urllib2.urlopen(inurl)
-# if you're using python 3, you should use
-# data = urllib.request.urlopen(inurl)
-
-docs = json.load(data)['response']['docs']
-# the ranking should start from 1 and increase
-rank = 1
-for doc in docs:
-    outf.write(qid + ' ' + 'Q0' + ' ' + str(doc['id']) + ' ' + str(rank) + ' ' + str(doc['score']) + ' ' + IRModel + '\n')
-    rank += 1
-outf.close()
+        docs = json.load(data)['response']['docs']
+        # the ranking should start from 1 and increase
+        rank = 1
+        for doc in docs:
+            if qid < 10:
+                outf.write("00" + str(qid) + ' ' + 'Q0' + ' ' + str(doc['id']) + ' ' + str(rank) + ' ' + str(doc['score']) + ' ' + IRModel + '\n')
+            rank += 1
+        qid += 1
+    outf.close()
