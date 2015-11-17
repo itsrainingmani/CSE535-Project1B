8b03922930da441511824aa92cfabc1caa7177e4
Manikandan <mtiruman@buffalo.edu>
2015-11-17 00:24:28 -0500

    Got the Scoring models to work

--- json_to_trec.py
+++ json_to_trec.py
@@ -9,16 +9,21 @@ import json
 # import urllib.request
 import urllib2
 
-
-with open('test_queries.txt', 'rb') as queries:
+arr = [" ", "text_en", "text_en","text_en", "text_de", "text_ru", "text_en", "text_en", "text", "text", "text", "text", "text_de", "text_en", "text_en"]
+with open('queries.txt', 'rb') as queries:
     # change the url according to your own koding username and query
     qid = 1
     for line in queries:
-        query = line[12:].rstrip()
-        inurl= "http://meganarutokage.koding.io:8983/solr/eval/select?q=*" + query + "*&fl=id%2Cscore&wt=json&indent=true&rows=1000"
+        query = line[4:].rstrip()
+        dubo = "00"
+
+        inurl= "http://meganarutokage.koding.io:8983/solr/eval/select?q=" + arr[qid] + ":" + query + "*&fl=id%2Cscore&wt=json&indent=true&rows=1000"
+        if (qid > 9):
+            dubo = "0"
+        inurl = urllib2.quote(inurl, "*:/&%?=._")
         outfn = 'BM25results.txt'
 
-        # change query id and IRModel name accordingly
+        print inurl
         IRModel= 'BM25'
         outf = open(outfn, 'a+')
         data = urllib2.urlopen(inurl)
@@ -29,8 +34,7 @@ with open('test_queries.txt', 'rb') as queries:
         # the ranking should start from 1 and increase
         rank = 1
         for doc in docs:
-            if qid < 10:
-                outf.write("00" + str(qid) + ' ' + 'Q0' + ' ' + str(doc['id']) + ' ' + str(rank) + ' ' + str(doc['score']) + ' ' + IRModel + '\n')
+            outf.write(dubo + str(qid) + ' ' + 'Q0' + ' ' + str(doc['id']) + ' ' + str(rank) + ' ' + str(doc['score']) + ' ' + IRModel + '\n')
             rank += 1
         qid += 1
     outf.close()
