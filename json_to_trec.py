0a44eb940b7f6d646570864d0a6321d8c5778a74
Manikandan <mtiruman@buffalo.edu>
2015-11-17 02:47:52 -0500

    Made a lot of changes to the Solr IR system

--- json_to_trec.py
+++ json_to_trec.py
@@ -9,19 +9,16 @@ import json
 # import urllib.request
 import urllib2
 
-arr = [" ", "text_en", "text_en","text_en", "text_de", "text_ru", "text_en", "text_en", "text", "text", "text", "text", "text_de", "text_en", "text_en"]
-with open('queries.txt', 'rb') as queries:
+arr = [" ", "text_en", "text_en", "text_en", "text_en", "text_en", "text_en", "text_en", "text_de", "text_de", "text_ru"]
+with open('test_queries.txt', 'rb') as queries:
     # change the url according to your own koding username and query
     qid = 1
     for line in queries:
-        query = line[4:].rstrip()
-        dubo = "00"
-
-        inurl= "http://meganarutokage.koding.io:8983/solr/eval/select?q=" + arr[qid] + ":" + query + "*&fl=id%2Cscore&wt=json&indent=true&rows=1000"
-        if (qid > 9):
-            dubo = "0"
+        query = line[12:].rstrip()
+        dubo = line[0:3]
+        inurl= "http://meganarutokage.koding.io:8983/solr/eval/select?q=" + query + "&fl=id%2Cscore&wt=json&indent=true&rows=1000"
         inurl = urllib2.quote(inurl, "*:/&%?=._")
-        outfn = 'BM25results.txt'
+        outfn = 'BM25results-2.txt'
 
         print inurl
         IRModel= 'BM25'
@@ -34,7 +31,7 @@ with open('queries.txt', 'rb') as queries:
         # the ranking should start from 1 and increase
         rank = 1
         for doc in docs:
-            outf.write(dubo + str(qid) + ' ' + 'Q0' + ' ' + str(doc['id']) + ' ' + str(rank) + ' ' + str(doc['score']) + ' ' + IRModel + '\n')
+            outf.write(dubo + ' ' + 'Q0' + ' ' + str(doc['id']) + ' ' + str(rank) + ' ' + str(doc['score']) + ' ' + IRModel + '\n')
             rank += 1
         qid += 1
     outf.close()
